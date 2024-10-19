973e5df15686fc96361d5b7a3e7cc99295cfaeb1
박준서 <pppg12jintpt@gmail.com>
2024-10-20 01:36:00 +0900

    feat:click to move

--- Assets/12.Tests/bakbak/script/ClickToMove.cs
+++ Assets/12.Tests/bakbak/script/ClickToMove.cs
@@ -4,12 +4,44 @@ using UnityEngine.InputSystem;
 
 public class ClickToMove : MonoBehaviour
 {
-    NavMeshAgent _agent;
+    private NavMeshAgent _agent;
+    private InputSystem_Actions _inputActions;
 
-    private void Start()
+    private Rigidbody _rb;
+
+    private void Awake()
     {
-        _agent = GetComponent<NavMeshAgent>();
+        _agent = GetComponent<NavMeshAgent>(); 
+
+        _inputActions = new InputSystem_Actions();
+        _inputActions.Player.Enable();
+        _inputActions.Player.Attack.performed += ClickToMoveCall;
+
+        _rb = GetComponent<Rigidbody>();
     }
 
-    
+    private void OnDestroy()
+    {
+        if (_inputActions != null)
+        {
+            _inputActions.Player.Attack.performed -= ClickToMoveCall;
+            _inputActions.Player.Disable();
+        }
+    }
+
+    private void Update()
+    {
+        print(_agent.velocity);
+    }
+
+    public void ClickToMoveCall(InputAction.CallbackContext context)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hitInfo))
+        {
+            Vector3 randomOffset = Random.insideUnitSphere * 0.5f;
+            randomOffset.y = 0;
+            _agent.destination = hitInfo.point + randomOffset;
+        }
+    }
 }
