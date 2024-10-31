132a559f905afbd34beacad63a59ccf64b6adb95
박준서 <pppg12jintpt@gmail.com>
2024-10-31 19:56:18 +0900

    feat:Player Flip

--- Assets/12.Tests/bakbak/script/PlayerFlip.cs
+++ Assets/12.Tests/bakbak/script/PlayerFlip.cs
@@ -1,7 +1,28 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.AI;
 
 public class PlayerFlip : MonoBehaviour
 {
     private NavMeshAgent _agent;
+    private float facingDirection = -1;
+
+    private void Awake()
+    {
+        _agent = GetComponentInParent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        if(Mathf.Abs(facingDirection + _agent.velocity.x) < 0.5f)
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        facingDirection *= -1;
+        transform.Rotate(new Vector3(0, 180f, 0));
+    }
 }
