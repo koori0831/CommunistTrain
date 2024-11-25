186dfee10d127ed608da7ddacd941d90ee21a92a
박준서 <pppg12jintpt@gmail.com>
2024-11-25 10:52:07 +0900

    feat:Bubble icon Pop complete

--- Assets/12.Tests/bakbak/script/ObjectInteract.cs
+++ Assets/12.Tests/bakbak/script/ObjectInteract.cs
@@ -5,26 +5,25 @@ using DG.Tweening;
 public class ObjectInteract : MonoBehaviour, IEnterInteractableHandler, IExitInteratableHandler
 
 {
-    private GameObject _interactionUI;
+    private Bubble _interactionUI;
     [SerializeField]
     private Vector3 effectSize;
     [SerializeField]
     private Vector3 defaltSize;
     [SerializeField]
     private CanvasControl _ticetCheckUI;
+    [SerializeField]
+    private Transform _bubblePosition;
 
     [SerializeField] 
     private float effectDuration;
 
-    public string PoolName => throw new System.NotImplementedException();
-
-    public GameObject ObjectPrefab => throw new System.NotImplementedException();
-
     public virtual void EnterInteraction()
     {
-        _interactionUI = PoolManager.Instance.Pop("Bubble").ObjectPrefab;
+        _interactionUI = PoolManager.Instance.Pop("Bubble") as Bubble;
         _interactionUI.gameObject.SetActive(false);
         _interactionUI.transform.SetParent(transform);
+        _interactionUI.transform.position = _bubblePosition.position;
         _interactionUI.transform.localScale = defaltSize;
         UIApear();
     }
@@ -32,14 +31,17 @@ public class ObjectInteract : MonoBehaviour, IEnterInteractableHandler, IExitInt
     public virtual void Interact()
     {
         print("³»¿ë¹° Æ¡");
-        _ticetCheckUI.FadeYoyo();
+        if (_interactionUI.gameObject.activeSelf == false&&
+            _ticetCheckUI != null)
+        {
+            _ticetCheckUI.FadeYoyo();
+        }
     }
 
     public virtual void ExitInteraction()
     {
         UIDisapear();
-        _interactionUI.gameObject.SetActive(false);
-        PoolManager.Instance.Push(_interactionUI.GetComponent<Ipoolable>());
+        
     }
 
     private void UIApear()
@@ -52,7 +54,8 @@ public class ObjectInteract : MonoBehaviour, IEnterInteractableHandler, IExitInt
     {
         Sequence sequence = DOTween.Sequence();
         sequence.Append(_interactionUI.transform.DOScale(defaltSize, effectDuration).SetEase(Ease.InQuint))
-            .AppendCallback(() => _interactionUI.gameObject.SetActive(false));
+            .AppendCallback(() => _interactionUI.gameObject.SetActive(false)).
+            AppendCallback(() => PoolManager.Instance.Push(_interactionUI));
         sequence.Play();
     }
 }
