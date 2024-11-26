4fc4840825d4acdd3660ecb8e8fe22f948ac6dae
koori0831 <fuyunokoori0831@gmail.com>
2024-11-26 10:54:56 +0900

    feat : Camera View

--- Assets/01.Scripts/koori/Door.cs
+++ Assets/01.Scripts/koori/Door.cs
@@ -1,10 +1,15 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class Door : ObjectInteract
 {
+    [SerializeField] private GameObject stationCam, Cam;
     public override void Interact()
     {
         base.Interact();
         Debug.Log("Reaching the moon");
+        stationCam.SetActive(true);
+
+        //Cam.transform.eulerAngles
     }
 }
---
+++ Assets/01.Scripts/koori/FlatformDoor.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class FlatformDoor : ObjectInteract
+{
+    public override void Interact()
+    {
+        base.Interact();
+        Debug.Log("기.. 기차아앗!");
+    }
+}
